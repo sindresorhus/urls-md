66841a3e99f340f5d7865bc2b314b5be012182fd
Sindre Sorhus <sindresorhus@gmail.com>
2015-03-04 18:48:49 +0800

    fix tests

--- test.js
+++ test.js
@@ -14,7 +14,7 @@ it('should extract links, get title, and convert to markdown links', function (c
 
 		assert.deepEqual(data, [
 			'[Yo Polymer – A Whirlwind Tour Of Web Component Tooling](http://updates.html5rocks.com/2014/01/Yo-Polymer-A-Whirlwind-Tour-Of-Web-Component-Tooling)',
-			'[Angular Fullstack 1.2.0 available now](http://tylerhenkel.com/angular-fullstack-1-2-0-available-now/)',
+			'[Angular Fullstack 1.2.0 available now](http://tylerhenkel.com/angular-fullstack-1-2-0-available-now)',
 			'![](https://github.global.ssl.fastly.net/images/modules/logos_page/GitHub-Mark.png)'
 		]);
 
